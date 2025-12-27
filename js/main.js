612c133fd2ba6531235618d2e04357a70c4010f9
Sri Kumaran <srikumaran260906@gmail.com>
2025-12-27 15:13:05 +0800

    animations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//scroll to sections using top nav bar, and update the section in top nav bar 
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll(".nav-links");
 
@@ -17,4 +18,65 @@ window.addEventListener("scroll", () => {
             link.classList.add("active");
         }
     });
+});
+
+//background and text gradient
+
+const bgStart = [255, 251, 0]; 
+const bgEnd   = [0, 200, 255];
+
+const textStart = [30, 30, 60];
+const textEnd   = [230, 230, 245];
+
+function lerp(a, b, t){
+    return a + (b-a) * t;
+}
+
+function lerpColor(c1, c2, t){
+    return `rgb(
+        ${Math.round(lerp(c1[0], c2[0], t))},
+        ${Math.round(lerp(c1[1], c2[1], t))},
+        ${Math.round(lerp(c1[2], c2[2], t))}
+    )`;
+}
+
+window.addEventListener("scroll", () => {
+  const t = window.scrollY /(document.body.scrollHeight - innerHeight);
+
+  document.body.style.background =
+    `linear-gradient(
+      180deg,
+      ${lerpColor(bgStart, bgEnd, t)},
+      ${lerpColor(bgEnd, bgStart, t)}
+    )`;
+//change this to edit the variable --primary and secondary text in css
+  document.querySelector(".text").style.color =
+    lerpColor(textStart, textEnd, t);
+});
+
+
+//canvas for bacground animation with scroll
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 1920;
+canvas.height=1080;
+const frames = [];
+const totalFrames = 251;
+
+for (let i = 0; i < totalFrames; i++) {
+  const img = new Image();
+
+  const frameNumber = String(i).padStart(4,"0")
+  img.src = `animation for website/${frameNumber}.png`;
+  frames.push(img);
+}
+
+window.addEventListener("scroll", () => {
+  const t = window.scrollY /
+    (document.body.scrollHeight - innerHeight);
+
+  const index = Math.floor(t * (totalFrames - 1));
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  ctx.drawImage(frames[index], 0, 0);
 });
